6a72c957608ab7e45ef4b547a82ab2e01f58da20
karol27119 <116905552+karol27119@users.noreply.github.com>
2023-09-06 12:21:34 +0000

    gastoscombustivel

--- App.js
+++ App.js
@@ -14,7 +14,15 @@ export default function App() {
     let intKmrodados = parseInt(kmrodados)
     let resultado = ((intLitro * intQuantabastecida) / intKmrodados);
     setResultado(resultado);
-  }
+  };
+
+  const limparDados = () => {
+    setLitro('');
+    setQuantabastecida('');
+    setKmrodados('');
+    setResultado('');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Autonomia do Ganho de Combustível</Text>
@@ -31,11 +39,19 @@ export default function App() {
         <TextInput value={kmrodados} onChangeText={setKmrodados} style={styles.caixa}/>
       </View>
       <View>
+        <Text>{" "}</Text>
         <Pressable onPress={() => calculo()}>
           <Text style={styles.calcul}>Calcular</Text>
         </Pressable>
       </View>
       <View>
+        <Text>{" "}</Text>
+        <Pressable onPress={() => limparDados()}>
+          <Text style={styles.calcul}>Limpar Dados</Text>
+        </Pressable>
+      </View>
+      <View>
+      <Text>{" "}</Text>
         <Text style={styles.caixresult}>{resultado}</Text>
       </View>
     </View>
@@ -48,28 +64,41 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:"#e8fdff",
   },
   titulo: {
-    fontSize:30,
+    fontSize:25,
     justifyContent:'center',
     alignItems:'center',
     alignSelf:'center',
     textAlign:'center',
     paddingBottom:20,
+    color:"#665c49",
   },
   valores:{
     fontSize:20,
     textAlign:'left',
+    color:"#665c49",
   },
   caixa:{
     borderWidth:1,
+    borderRadius:15,
+    borderColor:"#a69785",
   },
   calcul:{
     fontSize:20,
     textAlign:'center',
+    backgroundColor:"#807462",
+    width:150,
+    height:30,
+    borderRadius:30,
+    color:"white",
+    alignItems:"center",
+    textAlign:"center",
+    justifyContent:"center",
   },
   caixresult:{
     borderWidth:2,
-    borderColor:'green',
+    borderColor:'#665c49',
   }
 });
